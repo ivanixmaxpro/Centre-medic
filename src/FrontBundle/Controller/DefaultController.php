4cc21b7f2306cae447c550c3b64a249624a1ea3e
ivanixmaxpro <ivan.mir.014@gmail.com>
2017-02-14 15:31:49 +0100

    Estils

--- src/FrontBundle/Controller/DefaultController.php
+++ src/FrontBundle/Controller/DefaultController.php
@@ -1,8 +1,13 @@
 <?php
 
 namespace FrontBundle\Controller;
+use FrontBundle\Entity\Task;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class DefaultController extends Controller
 {
@@ -12,6 +17,19 @@ class DefaultController extends Controller
     }
     public function loginAction()
     {
-        return $this->render('FrontBundle:Default:login.html.twig');
+
+    	 // create a task and give it some dummy data for this example
+        $task = new Task();
+        $task->setTask('Write a blog post');
+        $task->setDueDate(new \DateTime('tomorrow'));
+
+        $form = $this->createFormBuilder()
+            ->add('text', 'text')
+            ->add('dueDate', 'date')
+            ->add('save', 'submit')
+            ->getForm();
+
+        return $this->render('FrontBundle:Default:login.html.twig', array(
+            'form' => $form->createView()));
     }
 }
