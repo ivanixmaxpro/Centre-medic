be0d760c3c3135bbc13d8acaae46582c72f8ad9d
ivanixmaxpro <ivan.mir.014@gmail.com>
2017-02-16 17:00:21 +0100

    Backend

--- src/BackBundle/Controller/DefaultController.php
+++ src/BackBundle/Controller/DefaultController.php
@@ -1,13 +1,35 @@
 <?php
 
-namespace BackBundle\Controller;
+namespace FrontBundle\Controller;
+use FrontBundle\Entity\Task;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('BackBundle:Default:index.html.twig');
+        return $this->render('FrontBundle:Default:index.html.twig');
+    }
+    public function loginAction()
+    {
+
+    	// create a task and give it some dummy data for this example
+        $task = new Task();
+        $task->setTask('Write a blog post');
+        $task->setDueDate(new \DateTime('tomorrow'));
+
+        $form = $this->createFormBuilder()
+            ->add('text', 'text')
+            ->add('dueDate', 'date')
+            ->add('save', 'submit')
+            ->getForm();
+
+        return $this->render('FrontBundle:Default:login.html.twig', array(
+            'form' => $form->createView()));
     }
 }
---
+++ src/BackBundle/Controller/PacientsController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace FrontBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class PacientsController extends Controller
+{
+	public function indexAction()
+	{
+		return $this->render('BackBundle:Default:index.html.twig');
+	}
+	public function newAction()
+	{
+		$pacient = new Pacient(); 
+		$form = $this->createForm(new PacientType(), $pacient );
+		$request = $this->getRequest(); 
+		$form->handleRequest($request); 
+		if($form->isSubmitted()){ 
+			if($form->isValid()){ 
+				$em = $this->getDoctrine()->getManager();
+				$em->persist($pacient); 
+				$em->flush();
+				return $this->redirect( 
+					$this->generateUrl('pacient_view', array('id' => $pacient )),301);
+			} 
+		} 
+
+		return $this->render('BackBundle:pacient:edit.html.twig', array( 'form' => $form->createView(), 'pacient' => $pacient)); 
+		}
+		public function viewAction($id)
+		{
+			$this->getDoctrine()->getRepository('BackBundle:pacient')->find($id); return $this->render('BackBundle:pacient:view.html.twig', array('prueba' => $prueba));
+		}
+		public function editAction()
+		{
+
+		}
+		public function deleteAction()
+		{
+
+		}
+	}
