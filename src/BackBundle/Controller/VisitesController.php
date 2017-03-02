18167f90e52afd531686b66ff0c2330297b81285
ivanixmaxpro <ivan.mir.014@gmail.com>
2017-03-02 19:44:08 +0100

    BackEnd casi finalizado

--- src/BackBundle/Controller/VisitesController.php
+++ src/BackBundle/Controller/VisitesController.php
@@ -2,7 +2,6 @@
 
 namespace BackBundle\Controller;
 
-
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use FrontBundle\Entity\Visites;
 use FrontBundle\Entity\Pacients;
@@ -14,122 +13,120 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 
-class VisitesController extends Controller
-{
-    public function llistaVisitesAction()
-    {
-    	$visites = $this->getDoctrine()->getRepository('FrontBundle:Visites')->findAll();
-        return $this->render('BackBundle:Default:visites.html.twig',array('visites' => $visites));
+class VisitesController extends Controller {
+
+    public function llistaVisitesAction() {
+        $visites = $this->getDoctrine()->getRepository('FrontBundle:Visites')->findAll();
+        return $this->render('BackBundle:Default:visites.html.twig', array('visites' => $visites));
     }
-    public function newAction(Request $request)
-    {
-			 // crea una categoria y le asigna algunos datos ficticios para este ejemplo
-    	$visites = new Visites();
 
-    	$em = $this->getDoctrine()->getEntityManager();
-    	$form = $this->createFormBuilder()
-    	->add('data', DateType::class, array('label' => 'Data'))
-    	->add('tractamentsFk',  ChoiceType::class, array('label' => 'Tractament',
-    		'choices'  => array(
-        	'Tractament' => $this->getDoctrine()->getRepository('FrontBundle:Tractaments')->findOneBy(array('tipus' => 'tractament')),
-       		'Concertada' => $this->getDoctrine()->getRepository('FrontBundle:Tractaments')->findOneBy(array('tipus' => 'concertada')),
-        	'Urgencia' => $this->getDoctrine()->getRepository('FrontBundle:Tractaments')->findOneBy(array('tipus' => 'urgencia')
-    	))))
-    	->add('metgesFk', TextType::class, array('label' => 'DNI Metge'))
-    	->add('pacientsFk', TextType::class, array('label' => 'DNI Pacient'))
-    	->add('save', SubmitType::class, array('label' => 'Afegir visita'))
-    	->getForm();
+    public function newAction(Request $request) {
+        // crea una categoria y le asigna algunos datos ficticios para este ejemplo
+        $visites = new Visites();
 
-    	$form->handleRequest($request);
+        $em = $this->getDoctrine()->getEntityManager();
+        $form = $this->createFormBuilder()
+                ->add('data', DateType::class, array('label' => 'Data'))
+                ->add('tractamentsFk', ChoiceType::class, array('label' => 'Tractament',
+                    'choices' => array(
+                        'Tractament' => $this->getDoctrine()->getRepository('FrontBundle:Tractaments')->findOneBy(array('tipus' => 'tractament')),
+                        'Concertada' => $this->getDoctrine()->getRepository('FrontBundle:Tractaments')->findOneBy(array('tipus' => 'concertada')),
+                        'Urgencia' => $this->getDoctrine()->getRepository('FrontBundle:Tractaments')->findOneBy(array('tipus' => 'urgencia')
+                ))))
+                ->add('metgesFk', TextType::class, array('label' => 'DNI Metge'))
+                ->add('pacientsFk', TextType::class, array('label' => 'DNI Pacient'))
+                ->add('save', SubmitType::class, array('label' => 'Afegir visita'))
+                ->getForm();
 
-    	if ($form->isSubmitted() && $form->isValid()) {
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
             // $form->getData() holds the submitted values
             // but, the original `$task` variable has also been updated
 
-    		$visites->setData($form->get('data')->getData());
-    		$visites->setTractamentsFk($form->get('tractamentsFk')->getData());
+            $visites->setData($form->get('data')->getData());
+            $visites->setTractamentsFk($form->get('tractamentsFk')->getData());
 
 
-    		// metges
-    		$dniM = $form->get('metgesFk')->getData();
-    		$metges = $em->getRepository('FrontBundle:Metges');
-    		$metge= $metges->findOneBy(array('dni' => $dniM));
-    		$visites->setMetgesFk($metge);
+            // metges
+            $dniM = $form->get('metgesFk')->getData();
+            $metges = $em->getRepository('FrontBundle:Metges');
+            $metge = $metges->findOneBy(array('dni' => $dniM));
+            $visites->setMetgesFk($metge);
 
-    		//pacients
-    		$dniP = $form->get('pacientsFk')->getData();
-    		$pacients = $em->getRepository('FrontBundle:Pacients');
-    		$pacient= $pacients->findOneBy(array('dni' => $dniP));
-    		$visites->setPacientsFk($pacient);
+            //pacients
+            $dniP = $form->get('pacientsFk')->getData();
+            $pacients = $em->getRepository('FrontBundle:Pacients');
+            $pacient = $pacients->findOneBy(array('dni' => $dniP));
+            $visites->setPacientsFk($pacient);
 
-    		//$visites = $form->getData();
-
+            //$visites = $form->getData();
             // ... perform some action, such as saving the task to the database
             // for example, if Category is a Doctrine entity, save it!
-    		$em = $this->getDoctrine()->getManager();
-    		$em->persist($visites);
-    		$em->flush();
-    		return $this->redirectToRoute('centre_medic_llista_back_visites');
-    	}
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($visites);
+            $em->flush();
+            return $this->redirectToRoute('centre_medic_llista_back_visites');
+        }
 
-    	return $this->render('BackBundle:Default:addPacient.html.twig', array(
-    		'titol' => 'Afegir Pacient',
-    		'form' => $form->createView(),
-    		));
+        return $this->render('BackBundle:Default:addPacient.html.twig', array(
+                    'titol' => 'Afegir Pacient',
+                    'form' => $form->createView(),
+        ));
     }
+
     // usar para mostrar la infd de algo en concreto
-		public function viewAction($id)
-		{
-			$this->getDoctrine()->getRepository('BackBundle:Visites')->find($id); 
-			return $this->render('BackBundle:visites:view.html.twig', array('prueba' => $prueba));
-		}
+    public function viewAction($id) {
+        $this->getDoctrine()->getRepository('BackBundle:Visites')->find($id);
+        return $this->render('BackBundle:visites:view.html.twig', array('prueba' => $prueba));
+    }
 
-		// /me he quedadoa aqui
-		public function editAction($id, Request $req)
-		{
- // crea una categoria y le asigna algunos datos ficticios para este ejemplo
-    	
-			$visites = $this->getDoctrine()->getRepository('FrontBundle:Visites')->find($id);
-			$form = $this->createFormBuilder()
-    	->add('data', DateType::class, array('label' => 'Data'))
-    	->add('tractamentsFk',  ChoiceType::class, array('label' => 'Tractament','data' => $visites->getData(),
-    		'choices'  => array(
-        	'Tractament' => $this->getDoctrine()->getRepository('FrontBundle:Tractaments')->findOneBy(array('tipus' => 'tractament')),
-       		'Concertada' => $this->getDoctrine()->getRepository('FrontBundle:Tractaments')->findOneBy(array('tipus' => 'concertada')),
-        	'Urgencia' => $this->getDoctrine()->getRepository('FrontBundle:Tractaments')->findOneBy(array('tipus' => 'urgencia')
-    	))
-    	->add('metgesFk', TextType::class, array('label' => 'DNI Metge' ,'data' => $visites->getMetgesFk()->toString())
-    	->add('pacientsFk', TextType::class, array('label' => 'DNI Pacient' ,'data' => $visites->getPacientsFk()->toString())
-    	->add('save', SubmitType::class, array('label' => 'Afegir visita'))
-    	->getForm();
+    // me he quedadoa aqui
+    public function editAction($id, Request $req) {
+        // crea una categoria y le asigna algunos datos ficticios para este ejemplo
 
-    	$form->handleRequest($req);
+        $visites = $this->getDoctrine()->getRepository('FrontBundle:Visites')->find($id);
+        $form = $this->createFormBuilder()
+                ->add('data', DateType::class, array('label' => 'Data' ,'data' => $visites->getData()))
+                ->add('tractamentsFk', ChoiceType::class, array('label' => 'Tractament',
+                    'choices' => array(
+                        'Tractament' => $this->getDoctrine()->getRepository('FrontBundle:Tractaments')->findOneBy(array('tipus' => 'tractament')),
+                        'Concertada' => $this->getDoctrine()->getRepository('FrontBundle:Tractaments')->findOneBy(array('tipus' => 'concertada')),
+                        'Urgencia' => $this->getDoctrine()->getRepository('FrontBundle:Tractaments')->findOneBy(array('tipus' => 'urgencia'))
+            ), 'data' => $visites->getTractamentsFk()))
+                ->add('metgesFk', TextType::class, array('label' => 'DNI Metge', 'data' => $visites->getMetgesFk()->getDni()))
+                ->add('pacientsFk', TextType::class, array('label' => 'DNI Pacient', 'data' => $visites->getPacientsFk()->getDni()))
+                ->add('save', SubmitType::class, array('label' => 'Afegir visita'))
+                ->getForm();
 
-    	if ($form->isSubmitted() && $form->isValid()) {
+        $form->handleRequest($req);
+
+        if ($form->isSubmitted() && $form->isValid()) {
             // $form->getData() holds the submitted values
             // but, the original `$task` variable has also been updated
 
-    		$visites = $form->getData();
+            $visites = $form->getData();
 
             // ... perform some action, such as saving the task to the database
             // for example, if Category is a Doctrine entity, save it!
-    		$em = $this->getDoctrine()->getManager();
-    		$em->persist($visites);
-    		$em->flush();
-    		return $this->redirectToRoute('centre_medic_llista_back_visites');
-    	}
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($visites);
+            $em->flush();
+            return $this->redirectToRoute('centre_medic_llista_back_visites');
+        }
 
-    	return $this->render('BackBundle:Default:addVisita.html.twig', array(
-    		'titol' => 'Afegir Pacient',
-    		'form' => $form->createView(),
-    		));
-		}
-		public function deleteAction($dni)
-		{
-			$visites = $this->getDoctrine()->getRepository('FrontBundle:Visites')->find($dni); 
-			$em = $this->getDoctrine()->getManager();
-    		$em->remove($visites);
-    		$em->flush();
-    		return $this->redirectToRoute('centre_medic_llista_back_visites');
-		}
+        return $this->render('BackBundle:Default:addVisita.html.twig', array(
+                    'titol' => 'Modificar Visita',
+                    'form' => $form->createView(),
+        ));
+    }
+
+    public function deleteAction($dni) {
+        $visites = $this->getDoctrine()->getRepository('FrontBundle:Visites')->find($dni);
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($visites);
+        $em->flush();
+        return $this->redirectToRoute('centre_medic_llista_back_visites');
+    }
+
 }
